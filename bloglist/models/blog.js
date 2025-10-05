47baaf8b9fd9b7b81aa682e313f7893dfa549b2c
Alexandre Matiello <alexandrematiellokf@gmail.com>
2025-10-04 21:08:29 -0300

    Fiz o 4.9, que era para garantir que os blogs retornados pela API tivessem o campo id em vez de _id, ajustando o modelo com o método toJSON e criando o teste correspondente.

--- bloglist/models/blog.js
+++ bloglist/models/blog.js
@@ -7,4 +7,13 @@ const blogSchema = new mongoose.Schema({
   likes: Number
 })
 
+// Transformação para JSON: troca _id por id e remove __v
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
 module.exports = mongoose.model('Blog', blogSchema)
--- bloglist/tests/blog_api.test.js
+++ bloglist/tests/blog_api.test.js
@@ -27,6 +27,7 @@ beforeEach(async () => {
   await Blog.insertMany(initialBlogs)
 })
 
+// Testes principais
 test('blogs are returned as json', async () => {
   await api
     .get('/api/blogs')
@@ -39,6 +40,40 @@ test('all blogs are returned', async () => {
   assert.strictEqual(response.body.length, initialBlogs.length)
 })
 
+beforeEach(async () => {
+  // setup para os testes adicionais
+  await Blog.deleteMany({})
+
+  const blogsForAdditionalTests = [
+    {
+      title: 'First blog',
+      author: 'Alex',
+      url: 'http://example.com/1',
+      likes: 5
+    },
+    {
+      title: 'Second blog',
+      author: 'Matiello',
+      url: 'http://example.com/2',
+      likes: 8
+    }
+  ]
+
+  await Blog.insertMany(blogsForAdditionalTests)
+})
+
+test('blogs are returned as json and have id field', async () => {
+  const response = await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  const blogs = response.body
+  assert.ok(blogs[0].id, 'blog should have an id field')
+  assert.strictEqual(blogs[0]._id, undefined, 'blog should not have _id field')
+})
+
+// Fecha conexão com Mongo após todos os testes
 after(async () => {
   await mongoose.connection.close()
 })
