44f4b9fb976695500f2c4d40826a517cbaf549b3
Alexandre Matiello <alexandrematiellokf@gmail.com>
2025-10-06 16:57:24 -0300

    Fiz a criação e listagem de usuários, que era para implementar o cadastro com senha criptografada e exibir todos via rota /api/users.

--- bloglist/app.js
+++ bloglist/app.js
@@ -3,10 +3,12 @@ const app = express()
 const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
 const config = require('./utils/config')
+const usersRouter = require('./controllers/users')
 
 mongoose.connect(config.MONGODB_URI)
 
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 module.exports = app
